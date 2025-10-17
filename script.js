ffa24387a85c8c0272bc8f7fcc66229a3906e53a
Prio_Prastyo. . . <74877643+prioprasty@users.noreply.github.com>
2025-10-18 00:36:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "prio";
+  sender = "morpheus";
 }
 
 let footer = document.getElementById("credit");
